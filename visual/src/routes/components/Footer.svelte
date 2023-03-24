535a250e527ae2e434f9780db3d2bce63a230c51
PI1T21 <121205933+PI1T21@users.noreply.github.com>
2023-03-24 15:30:03 -0300

    Delete visual directory

--- visual/src/routes/components/Footer.svelte
+++
@@ -1,3 +0,0 @@
-<p class="bg-black text-white relative">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa vero cupiditate mollitia nesciunt, sequi laboriosam sit quaerat delectus cumque! Maiores, minus recusandae. Placeat facere voluptatum, totam aliquid exercitationem omnis et!
-</p>
--- visual/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { defineConfig, UserConfig } from 'vite';
-
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
-export default config;
